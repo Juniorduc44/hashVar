ba0a6e7012ebc7c8f0acd1df5d6b9cb8a10c0e5e
Junior Ducatel <Juniorduc44@gmail.com>
2023-11-22 19:57:03 -0800

    v0.0.1

---
+++ hashVar.py
@@ -0,0 +1,24 @@
+#Verify SHA256
+
+
+
+
+
+
+import hashlib
+
+original = input("Enter the original 256 hash: ")
+file = input("Enter the file and its path: ")
+def calculate_sha256(file_path):
+    try:
+
+        with open(file,"rb") as f:
+            bytes = f.read() # read entire file as bytes
+            readable_hash = hashlib.sha256(bytes).hexdigest();
+            return readable_hash
+    except FileNotFoundError:
+        return "File not found"
+
+# Replace 'yourfile.txt' with the name of your file
+print(calculate_sha256('yourfile.txt'))
+print(original)
